8f1ea01f6f08130fb86665c1a1da5519a386d74e
Marek <marek.brynda96@gmail.com>
2020-04-01 20:45:20 +0200

    download list of pages and scrap their keys

--- 03/get_keys_pages.py
+++ 03/get_keys_pages.py
@@ -1,25 +1,37 @@
 import numpy as np
 from subprocess import getoutput
+import requests
 import sys
 import ssl
+import pandas as pd
 from OpenSSL import crypto
-#https://security.stackexchange.com/questions/16085/how-to-get-public-key-of-a-secure-webpage
 
 hostname = 'www.facebook.com'
 port = 443
 
-key = getoutput('openssl s_client -connect '+hostname+':'+str(port)+' | openssl x509 -pubkey -noout')
-print(key)
+#key = getoutput('openssl s_client -connect '+hostname+':'+str(port)+' | openssl x509 -pubkey -noout')
+#print(key)
 
-#https://stackoverflow.com/questions/41171891/how-to-download-x509-certificate-using-python
-cert = ssl.get_server_certificate((hostname, port))
+#https://www.domcop.com/top-10-million-domains
 
-#https://stackoverflow.com/questions/41172839/how-to-convert-the-x509-get-pubkey-to-hexadecimal-in-python
-#https://stackoverflow.com/questions/7689941/how-can-i-retrieve-the-tls-ssl-peer-certificate-of-a-remote-host-using-python
-x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
-key = crypto.dump_publickey(crypto.FILETYPE_PEM,x509.get_pubkey())
-key = key.decode()
-print(key)
+pd_pages = pd.read_csv('top10milliondomains.csv', index_col= 0)
+list_pages = pd_pages.Domain.values.tolist()
 
+key_page = {}
 
-#https://stackoverflow.com/questions/10362965/how-to-get-public-key-using-pyopenssl
+for i, p in eumerate(list_pages):
+    if i % 100 == 0:
+        print(i)
+    
+    cert = ssl.get_server_certificate((p, port))
+
+    x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+    key = crypto.dump_publickey(crypto.FILETYPE_PEM,x509.get_pubkey())
+    key_decode = key.decode()
+
+    if key_decode in wordFreqDic:
+        key_page[key_decode] = key_page[key_decode] + [p]
+    else:
+        key_page[key_decode] = [p]
+
+print(len(key_page))
