2e9b491863b84b0f64a1776254d2b1d77749e64a
Marek <marek.brynda96@gmail.com>
2020-04-02 08:22:12 +0200

    get keys adn save over 1000 to csv

--- 03/get_keys_pages.py
+++ 03/get_keys_pages.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import numpy as np
 from subprocess import getoutput
 import requests
@@ -5,6 +8,7 @@ import sys
 import ssl
 import pandas as pd
 from OpenSSL import crypto
+import concurrent.futures
 
 hostname = 'www.facebook.com'
 port = 443
@@ -14,24 +18,56 @@ port = 443
 
 #https://www.domcop.com/top-10-million-domains
 
-pd_pages = pd.read_csv('top10milliondomains.csv', index_col= 0)
-list_pages = pd_pages.Domain.values.tolist()
-
 key_page = {}
 
-for i, p in eumerate(list_pages):
-    if i % 100 == 0:
-        print(i)
-    
-    cert = ssl.get_server_certificate((p, port))
+j = 0
+nrows = 1000
 
-    x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
-    key = crypto.dump_publickey(crypto.FILETYPE_PEM,x509.get_pubkey())
-    key_decode = key.decode()
+n_list = [i for i in range(nrows, 10**7//nrows, nrows)]
 
-    if key_decode in wordFreqDic:
-        key_page[key_decode] = key_page[key_decode] + [p]
-    else:
-        key_page[key_decode] = [p]
+print(n_list)
 
-print(len(key_page))
+def process_keys(n = 0):
+    global j 
+
+    if n == 0: 
+        n = 1
+    pd_pages = pd.read_csv('top10milliondomains.csv', skiprows=n, nrows=nrows, names= ['Rank', 'Domain', 'OpenRank'], header=None)
+    pd_pages['Key'] = np.nan
+
+    for i, p in pd_pages.iterrows():
+        
+        if (p['Rank']) % 10 == 0:
+            print(n, i, j, end = '\r')
+        
+        hostname = 'www.' + p['Domain'] 
+
+        try:
+            cert = ssl.get_server_certificate((hostname, port))
+
+            x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
+            key = crypto.dump_publickey(crypto.FILETYPE_PEM,x509.get_pubkey())
+            #print(key)
+
+            #key_decode = key.decode()
+
+            #print(key_decode)
+
+            j += 1
+
+            pd_pages[i, 'Key'] =  key
+
+        except:
+            pass
+            #key_decode = np.nan
+        
+        '''
+        if key_decode in key_page:
+            key_page[key_decode] = key_page[key_decode] + [hostname]
+        else:
+            key_page[key_decode] = [hostname]
+        '''
+    pd_pages.to_csv('keys/pages_with_keys'+str(n)+'.csv')
+
+for n in n_list:
+    process_keys(n)
