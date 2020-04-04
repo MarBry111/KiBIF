c3880d16eadf01268f81ad195f0d3bc05c62b160
Marek <marek.brynda96@gmail.com>
2020-04-04 12:42:24 +0200

    lab03 - repair error too big for float

--- 03/batch_gdc.py
+++ 03/batch_gdc.py
@@ -1,22 +1,24 @@
 import numpy as np
+from math import gcd 
 
 def product(X):
 	if len(X) == 0: return 1
 	while len(X) > 1:
-		X = [int(np.prod(X[int(i*2):int((i+1)*2)])) for i in range(int((len(X)+1)//2))]
+		X = [int(np.prod(X[int(i*2):int((i+1)*2)])) for i in range((len(X)+1)//2)]
 	return X[0]
 
 def producttree(X):
 	result = [X]
 	while len(X) > 1:
-		X = [int(np.prod(X[int(i*2):int((i+1)*2)])) for i in range(int((len(X)+1)//2))]
+		print((len(X)+1)//2)
+		X = [(lambda i: np.prod(X[int(i*2):int((i+1)*2)]) )(i) for i in range((len(X)+1)//2)]
 		result.append(X)
 	return result
 
 def remaindersusingproducttree(n,T):
 	result = [n]
 	for t in reversed(T):
-		result = [result[floor(i/2)] % t[i] for i in range(len(t))]
+		result = [result[floor(i//2)] % t[i] for i in range(len(t))]
 	return result
 
 def remainders(n,X):
@@ -27,5 +29,5 @@ def batchgcd_faster(X):
 	R = prods.pop()
 	while prods:
 		X = prods.pop()
-		R = [R[int(floor(i/2))] % X[i]**2 for i in range(len(X))]
-	return [gcd(r/n,n) for r,n in zip(R,X)]
+		R = [R[int(i//2)] % X[i]**2 for i in range(len(X))]
+	return [ gcd(r//n,n) for r,n in zip(R,X)]
